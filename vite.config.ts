206a347472abfe66e8dbfc42a1606aaafa0ce64a
Novarc <2124363741@qq.com>
2024-06-22 22:04:58 +0800

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
@@ -11,7 +10,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
     AutoImport({resolvers: [ElementPlusResolver()],}),
     Components({
 	    resolvers: [ElementPlusResolver()],}),
